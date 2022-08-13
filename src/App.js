480e4bfe111fee37f056f95541c72f2b2ef42dad
mohsinimtiaz4026 <businesstycon941@gmail.com>
2022-08-13 15:18:28 +0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import Index from './Routers';
+
+// import scss
+import "./assets/scss/hope-ui.scss";
+import "./assets/scss/dark.scss";
+import "./assets/scss/rtl.scss";
+import "./assets/scss/custom.scss";
+import "./assets/scss/customizer.scss";
+
+
+function App() {
+  return (
+    <Index />
+  );
+}
+
+export default App;
